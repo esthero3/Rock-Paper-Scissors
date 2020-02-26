ec064eb9a99b72e1fb0a4a5cbacbd6a2be683cbe
esthero3 <54954631+esthero3@users.noreply.github.com>
2020-02-26 11:59:17 -0700

    almost done setting up

---
+++ RockPaperScissorsChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RockPaperScissorsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
