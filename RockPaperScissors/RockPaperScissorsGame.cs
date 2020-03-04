fe13e8cf4ad420e0294522181d1205358f8f6d96
esthero3 <54954631+esthero3@users.noreply.github.com>
2020-03-04 11:00:21 -0700

    Done

--- RockPaperScissors/RockPaperScissorsGame.cs
+++ RockPaperScissors/RockPaperScissorsGame.cs
@@ -4,6 +4,7 @@ namespace RockPaperScissors
 {
     public class RockPaperScissorsGame
     {
+        //variables
         private Random rng;
         private int wins;
         private int losses;
@@ -14,6 +15,7 @@ namespace RockPaperScissors
             rng = new Random();
         }
 
+        //method to play the game
         public void Play()
         {
             string userChoice;
@@ -21,26 +23,75 @@ namespace RockPaperScissors
 
             while (userChoice != "Q")
             {
-                //do stuff to play gamee
-                //Console.WriteLine("We be palyin");
-
                 string computerChoice = GetComputerChoice();
+                DetermineWinner(userChoice, computerChoice);
+                PrintScore();
+                Console.Clear();
                 userChoice = PromptUser();
             }
         }
 
+        //method to print score
+        private void PrintScore()
+        {
+            Console.WriteLine();
+            Console.WriteLine("wins: {0}", wins);
+            Console.WriteLine("losses: {0}", losses);
+            Console.WriteLine("ties: {0}", ties);
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+        }
+
+            //determines the winner of the round
+            private void DetermineWinner(String userChoice, string computerChoice)
+            {
+                if (userChoice == computerChoice)
+                {
+                    ties++;
+                    Console.WriteLine("You both picked {0}, you tied!", ConvertChoiceToWord(userChoice));
+                } 
+                else if (userChoice == "R" && computerChoice == "S" || userChoice == "S" && computerChoice == "P" || userChoice == "P" && computerChoice == "R")
+                {
+                    wins++;
+                    Console.WriteLine("You picked {0} and the computer picked {1}, you win! attaboy!", ConvertChoiceToWord(userChoice), ConvertChoiceToWord(computerChoice));
+                }
+                else 
+                {
+                    losses++;
+                    Console.WriteLine("You picked {0} and the computer picked {1}, you lose! L!", ConvertChoiceToWord(userChoice), ConvertChoiceToWord(computerChoice));
+                }
+            }
+
+        //gets the computer's choice
         private string GetComputerChoice()
         {
             int choice = rng.Next(1, 4);
 
             if (choice == 1)
-                return "R";
-            else if (choice == 2)
-                return "P";
-            else
+            {
+               return "R"; 
+            } else if (choice == 2)
+            {
+              return "P";  
+            } else 
+            {
                 return "S";
+            }       
         }
 
+        //converts the letters to actual words. Makes the app cleaner/more understandable
+        private string ConvertChoiceToWord(string choice)
+        {
+            if (choice == "R")
+            return "Rock";
+            else if (choice == "S")
+            return "Scissors";
+            else 
+            return "Paper";
+        }
+
+        //prompts the user
         private string PromptUser()
         {
             while(true)
--- RockPaperScissorsChallenge/Program.cs
+++ RockPaperScissorsChallenge/Program.cs
@@ -7,6 +7,7 @@ namespace RockPaperScissorsChallenge
         static void Main(string[] args)
         {
             // Namespace.Class ObjectName = new Namespace.Class()
+            //dotnet run RockPaperScissorsChallenge
       RockPaperScissors.RockPaperScissorsGame game = new RockPaperScissors.RockPaperScissorsGame();
       game.Play();
         }
